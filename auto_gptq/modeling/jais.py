1c9371cbc0da10a9922327cfec9ed6684b2643af
Abu Bakr Soliman <bakrianoo@gmail.com>
2023-10-31 19:41:03 +0200

    Supporting JAIS Model

---
+++ auto_gptq/modeling/jais.py
@@ -0,0 +1,16 @@
+from ._base import *
+
+class JAISGPTQForCausalLM(BaseGPTQForCausalLM):
+    layer_type = "JAISBlock"
+    layers_block_name = "transformer.h"
+    outside_layer_modules = [
+        "transformer.wte", "transformer.ln_f", "transformer.relative_pe"
+    ]
+    inside_layer_modules = [
+        ["ln_1"],
+        ["attn.c_attn", "attn.c_proj"],
+        ["ln_2"],
+        ["mlp.c_fc", "mlp.c_fc2", "mlp.c_proj"]
+    ]
+
+__all__ = ["JAISGPTQForCausalLM"]
